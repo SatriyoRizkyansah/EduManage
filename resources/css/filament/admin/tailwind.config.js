744b0ba8c24e9b94be9d847840ac3092db75ce8a
Satriyo Rizkyansah <satttrioo@gmail.Com>
2024-08-25 18:55:38 +0700

    add tailwind and edit home page

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -1,10 +1,33 @@
-import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
+import preset from "../../../../vendor/filament/filament/tailwind.config.preset";
 
 export default {
     presets: [preset],
+    // content: [
+    //     "./app/Filament/**/*.php",
+    //     "./resources/views/filament/**/*.blade.php",
+    //     "./vendor/filament/**/*.blade.php",
+    //     "./resources/**/*.blade.php",
+    //     "./resources/**/*.js",
+    //     "./resources/**/*.vue",
+    // ],
     content: [
-        './app/Filament/**/*.php',
-        './resources/views/filament/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
+        "./app/Filament/**/*.php",
+        "./resources/views/filament/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
-}
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+
+// module.exports = {
+//     content: ["./src/**/*.{html,js}"],
+//     theme: {
+//         extend: {},
+//     },
+//     plugins: [],
+// };
